647eb6d8682ab85d99eb1266616d0a30967e323c
Sulaimon Zafarov <sulaymonwork@gmail.com>
2021-10-29 18:17:17 +0500

    Add project files.

---
+++ HW-mvc-part-1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using HW_mvc_part_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Logging;
+
+namespace HW_mvc_part_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly string _conString = "Data source = localhost; Initial catalog = AcademySummer; Integrated Security = True";
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            List<Person> people = new List<Person>();
+            using (IDbConnection con = new SqlConnection(_conString))
+            {
+                people = con.Query<Person>("Select * from Person").ToList();
+            }
+            return View(people);
+        }
+    }
+}
---
+++ HW-mvc-part-1/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW_mvc_part_1.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+    }
+    
+}
